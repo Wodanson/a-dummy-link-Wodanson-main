d0e815cc3807eaf830f609e61fddfefab1fba057
Wodanson <1782819gillissen@zuyd.nl>
2023-03-02 12:42:01 +0100

    h

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 const element = document.querySelector("li");
-element.innerHTML = "Read more about <a title='go to https://theitcrowd.fandom.com/wiki/Jen_Barber'
-href ='https://theitcrowd.fandom.com/wiki/Jen_Barber'> Jen </a>" ;
+element.innerHTML = "Read more about <a title='go to https://theitcrowd.fandom.com/wiki/Jen_Barber' href ='https://theitcrowd.fandom.com/wiki/Jen_Barber'> Jen </a>";
 
 
 
