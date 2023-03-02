04f345f7edacf1aca18b721fa8134940fb8fde03
Wodanson <1782819gillissen@zuyd.nl>
2023-03-02 11:18:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// put your code here
+
+const element = document.querySelector("li");
+element.innerHTML = "Read more about <a title='go to https://theitcrowd.fandom.com/wiki/Jen_Barber'
+href ='https://theitcrowd.fandom.com/wiki/Jen_Barber'> Jen </a>" ;
+
+
+
+
